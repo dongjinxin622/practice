0c9ebc3d7ba1b0e40782669daa0618cba68c7f62
root <root@Omen10.localdomain>
2025-07-20 21:18:42 +0800

    generate web app in app folder

--- app/src/calculator.py
+++ app/src/calculator.py
@@ -1,9 +1,9 @@
 class Calculator():
     def add(self, a, b):
         return a + b
-    def substract(self, a, b):
+    def subtract(self, a, b):
         return a - b
-    def muliply(self, a, b):
+    def multiply(self, a, b):
         return a * b
     def divide(self, a, b):
         if b == 0:
